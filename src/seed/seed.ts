d1cad7ef8b1a2820ff1e0e76a22d8ef060937337
Marcin Wydrzyński <wydrzynskimarcin@gmail.com>
2018-06-23 18:51:15 +0200

    t

--- src/seed/seed.ts
+++ src/seed/seed.ts
@@ -3,14 +3,14 @@ import { exec as execCallback } from "child_process"
 import { Connection, createConnection } from "typeorm"
 import { promisify } from "util"
 
+import * as path from "path"
 import dbConfig from "../../ormconfig"
 import seedCompany from "./seedCompany"
 import seedUsers from "./seedUsers"
 
-
 const exec = promisify(execCallback)
 
-exec(`npm run ts-node node_modules\\typeorm\\cli.js schema:drop`)
+exec(`npm run ts-node ${path.join(__dirname, '..', '..', 'node_modules', 'typeorm', 'cli.js')} schema:drop`)
     .then(() => exec(`npm run ts-node node_modules\\typeorm\\cli.js schema:sync`))
     .then(() => createConnection(<any>dbConfig))
     .then(seed)
