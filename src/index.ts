e32e08d4296b2fa18cfee1134f670fad0211feca
Marcin Wydrzyński <wydrzynskimarcin@gmail.com>
2018-06-24 13:26:35 +0200

    d

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,5 @@ import appPromise from "../src/app"
 
 appPromise.then(({ app }) => {
     app.listen(process.env.PORT || 3000)
-    console.log('listening')
+    console.log(`listening on port ${process.env.PORT || 3000}`)
 })
